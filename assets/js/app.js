ced85c1ccefce492e8c3a315b9be6d161bb1320e
oyasumi02 <jaguilar1999@live.com>
2025-07-02 03:46:10 -0500

    Created random word card in projects.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,8 @@
-let x = "Test";
-console.log(x);
+/* Random Word selector */
+let list = ["Paige", "Hello!", "Banana", "Leave this page right now", "Idk", "Something"];
+let random = Math.random();
+
+window.onload = function() {
+    let randomWord = Math.floor(Math.random() * list.length);
+    document.getElementById("project1").textContent = list[randomWord];
+};
