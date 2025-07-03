93eb29ee2d7ae4952f43e8c7c977cb1abba1f494
oyasumi02 <jaguilar1999@live.com>
2025-07-02 22:41:58 -0500

    Added more random elements to projects.html

--- assets/js/random_element.js
+++ assets/js/random_element.js
@@ -3,6 +3,11 @@ let list = ["Paige", "Hello!", "Banana", "Leave this page right now", "Idk", "So
 let random = Math.random();
 
 window.onload = function() {
+    let maxNum = 1000000;
+
     let randomWord = Math.floor(Math.random() * list.length);
+    let randomNum = Math.floor(Math.random() * maxNum);
+
     document.getElementById("project1").textContent = list[randomWord];
+    document.getElementById("number").textContent = randomNum.toString();
 };
